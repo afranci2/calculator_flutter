8f4b965190bd65658825073f35736826d2b83469
El Samp <anthony.francisco.aoct@gmail.com>
2024-09-12 10:54:46 -0400

    name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Simple Calculator by [Your Name]'),
+      home: const MyHomePage(title: 'Simple Calculator - Anthony Francisco'),
     );
   }
 }
